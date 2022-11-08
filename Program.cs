c34f7af43eed5d84ad86c8a23c6d17261b1d2d15
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-08 17:45:24 +0530

    check for leap year or not

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace leapyearw1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int year;
+
+            Console.WriteLine("enter a year");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4) == 0)
+                Console.WriteLine("{0} is a leap year", year);
+            else if ((year % 100) == 0)
+                Console.WriteLine("{0} is not a leap year", year);
+            else if ((year % 400) == 0)
+                Console.WriteLine("{0} is a leap year", year);
+            else
+                Console.WriteLine("{0} is not a leap year", year);
+        }
+    }
+}
